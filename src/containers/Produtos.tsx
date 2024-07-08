40df996a355571224c726218b00020f5d2ffec22
Ana Alice <anaeanali5@gmail.com>
2024-07-08 13:47:37 -0300

    ajustanto

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
@@ -7,13 +8,49 @@ import * as S from './styles'
 import { RootReducer } from '../store'
 
 const ProdutosComponent = () => {
-  const { data: produtos } = useGetProdutosQuery()
+  const { data: produtos, isLoading, isError } = useGetProdutosQuery()
   const favoritos = useSelector((state: RootReducer) => state.favoritos.itens)
+  const [showProdutos, setShowProdutos] = useState(false)
+  const [loadingComplete, setLoadingComplete] = useState(false)
+
+  useEffect(() => {
+    const minLoadingTime = 2000
+    const timer = setTimeout(() => {
+      setLoadingComplete(true)
+    }, minLoadingTime)
+
+    if (!isLoading) {
+      clearTimeout(timer)
+      setLoadingComplete(true)
+    }
+
+    return () => clearTimeout(timer)
+  }, [isLoading])
+
+  useEffect(() => {
+    if (loadingComplete) {
+      const displayTimer = setTimeout(() => {
+        setShowProdutos(true)
+      }, 2000)
+
+      return () => clearTimeout(displayTimer)
+    }
+  }, [loadingComplete])
+
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
     const productID = produto.id
     const idsDosFavoritos = favoritos.map((f) => f.id)
     return idsDosFavoritos.includes(productID)
   }
+
+  if (!showProdutos) {
+    return <div>Carregando...</div>
+  }
+
+  if (isError) {
+    return <div>Erro ao carregar produtos</div>
+  }
+
   return (
     <>
       <S.Produtos>
